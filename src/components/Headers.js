c460ef08e61892393da2547b35e2f8921371ec2f
avani_baheti <avanibaheti7@gmail.com>
2023-08-21 20:37:53 +0530

    changed

---
+++ src/components/Headers.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+const Headers = () => {
+  return (
+    <div>
+
+<Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Pagee</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+
+    </div>
+  )
+}
+
+export default Headers
